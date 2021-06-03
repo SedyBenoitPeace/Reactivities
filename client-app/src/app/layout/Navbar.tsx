93330f0505754faf8840a350ca62a71dc6e3f0ba
Stefano Rifici <stefanorifici@gamestop.com>
2021-06-03 16:10:17 +0100

    Folder Structure and Navbar

---
+++ client-app/src/app/layout/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Container, Menu, MenuItem } from 'semantic-ui-react';
+
+export default function Navbar(){
+    return (
+        <Menu inverted fixed='top'>
+            <Container>
+                <Menu.Item header>
+                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
+                    Reactivities
+                </Menu.Item>
+                <Menu.Item name="Activities"/>
+                <Menu.Item>
+                    <Button positive content='Create Activity'/>
+                </Menu.Item>
+            </Container>
+        </Menu>
+    )
+}
