16bfad6afa46c0d2e744deb944ee06962ac4ca32
Stefano Rifici <stefanorifici@gamestop.com>
2021-06-25 15:59:52 +0100

    Section 5 till 54 video

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,26 +3,49 @@ import axios from 'axios';
 import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import Navbar from './Navbar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
       setActivities(response.data);
     })
   }, [])
 
+  function handleSelectActivity(id: string){
+    setSelectedActivity(activities.find(activity=>activity.id === id))
+  }
+
+  function handleCancelSelectActivity(){
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string){
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose(){
+    setEditMode(false);
+  }
+
   return (
     <Fragment>
-        <Navbar />
+        <Navbar openForm={handleFormOpen}/>
         <Container style={{marginTop: '7em'}}>
-          <List>
-          {activities.map((activity) => (
-              <List.Item key={activity.id}>
-                {activity.title}
-              </List.Item>
-            ))}
-          </List>
+          <ActivityDashboard 
+            activities={activities}
+            selectedActivity={selectedActivity}
+            selectActivity={handleSelectActivity}
+            cancelSelectActivity={handleCancelSelectActivity}
+            editMode = {editMode}
+            openForm = {handleFormOpen}
+            closeForm = {handleFormClose}
+          />
         </Container>
     </Fragment>
   );
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Grid, List } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+import ActivityList from "./ActivityList";
+
+interface Props {
+    activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+}
+
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm }: Props) {
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList activities={activities} selectActivity={selectActivity} />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && !editMode &&
+                    <ActivityDetails activity={selectedActivity}
+                        cancelSelectActivity={cancelSelectActivity}
+                        openForm={openForm}
+                    />}
+                {editMode &&
+                    <ActivityForm closeForm={closeForm} activity={selectedActivity} />}
+            </Grid.Column>
+        </Grid>
+    )
+}
