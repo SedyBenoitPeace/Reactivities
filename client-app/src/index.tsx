5b2691826b3720108d0480b8e598ec5c34c57630
Stefano Rifici <stefano.rifici@gmail.com>
2022-02-16 00:05:33 +0100

    End of section 8 with react router 6

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,10 +5,13 @@ import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
