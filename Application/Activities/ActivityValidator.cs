c2114a84898e47a6ae1f1ce816b010840f56cddc
Stefano Rifici <stefano.rifici@gmail.com>
2022-03-15 21:28:10 +0100

    Missing section 10 files

---
+++ Application/Activities/ActivityValidator.cs
@@ -0,0 +1,18 @@
+using Domain;
+using FluentValidation;
+
+namespace Application.Activities
+{
+    public class ActivityValidator : AbstractValidator<Activity>
+    {
+        public ActivityValidator()
+        {
+            RuleFor(activity => activity.Title).NotEmpty();
+            RuleFor(activity => activity.Description).NotEmpty();
+            RuleFor(activity => activity.Date).NotEmpty();
+            RuleFor(activity => activity.Category).NotEmpty();
+            RuleFor(activity => activity.City).NotEmpty();
+            RuleFor(activity => activity.Venue).NotEmpty();
+        }
+    }
+}
