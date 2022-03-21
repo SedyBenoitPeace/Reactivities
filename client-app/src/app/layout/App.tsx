a2504871cea2284d408e338b178a4564ec8c4274
Stefano Rifici <stefano.rifici@gmail.com>
2022-03-21 23:30:39 +0100

    Section_10_in_progress

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,9 +4,12 @@ import Navbar from './Navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
-import { Outlet, Route, Routes, useLocation } from 'react-router';
+import { Route, Routes, useLocation } from 'react-router';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import TestErrors from '../../features/errors/TestErrors';
 
 function App() {
 
@@ -14,7 +17,7 @@ function App() {
 
   return (
     <Fragment>
-      {/* <Navbar /> */}
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Container style={{ marginTop: '7em' }}>
         <Routes>
           <Route path='/' element={<HomePage />} />
@@ -26,6 +29,8 @@ function App() {
                 <Route key={location.key} path={path} element={<ActivityForm key={location.key} />} />
               );
             })}
+            <Route path='/errors' element={<TestErrors />} />
+            <Route path='/not-found' element={<NotFound />} />
           </Route>
         </Routes>
       </Container>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'react-calendar/dist/Calendar.css'
+import 'react-toastify/dist/ReactToastify.min.css'
 import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { BrowserRouter, Routes } from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import {unstable_HistoryRouter as HistoryRouter} from 'react-router-dom';
+
+export const history = createBrowserHistory();
+
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <BrowserRouter>
+    <HistoryRouter history={history}>
         <App />
-    </BrowserRouter>
+    </HistoryRouter>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
