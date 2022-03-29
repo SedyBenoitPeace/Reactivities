3d2bf81214868627672f53fd166ba3cbd4efb64c
Stefano Rifici <srifici@technogym.com>
2022-03-29 12:47:37 +0200

    End of section 14

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { history } from "../..";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../models/user";
+import { config } from "process";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -12,6 +13,15 @@ const sleep = (delay: number) => {
 }
 
 axios.defaults.baseURL = "http://localhost:5000/api";
+
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
 axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
@@ -69,8 +79,8 @@ const Activities = {
 
 const Account = {
     current: () => requests.get<User>('/account'),
-    login: (user: UserFormValues) => requests.post('/account/login', user),
-    register: (user: UserFormValues) => requests.post('/account/register', user),
+    login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+    register: (user: UserFormValues) => requests.post<User>('/account/register', user),
 }
 
 const agent = {
--- client-app/src/app/layout/Navbar.tsx
+++ client-app/src/app/layout/Navbar.tsx
@@ -1,27 +1,37 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import { Button, Container, Menu } from 'semantic-ui-react';
-//import { useStore } from '../stores/store';
+import { Button, Container, Menu, Image, Dropdown } from 'semantic-ui-react';
+import { useStore } from '../stores/store';
 
-export default function Navbar() {
-    //const { activityStore } = useStore();
+export default observer(function Navbar() {
+    const { userStore: { user, logout } } = useStore();
 
     return (
         <>
-        <Menu inverted fixed='top'>
-            <Container>
-                <Menu.Item as={NavLink} to='/' header>
-                    <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
-                    Reactivities
-                </Menu.Item>
-                <Menu.Item as={NavLink} to='/activities' name="Activities" />
-                <Menu.Item as={NavLink} to='/errors' name="Errors" />
-                <Menu.Item>
-                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
-                </Menu.Item>
-            </Container>
-        </Menu>
-        <Outlet />
+            <Menu inverted fixed='top'>
+                <Container>
+                    <Menu.Item as={NavLink} to='/' header>
+                        <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
+                        Reactivities
+                    </Menu.Item>
+                    <Menu.Item as={NavLink} to='/activities' name="Activities" />
+                    <Menu.Item as={NavLink} to='/errors' name="Errors" />
+                    <Menu.Item>
+                        <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
+                    </Menu.Item>
+                    <Menu.Item position='right'>
+                        <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
+                        <Dropdown pointing='top left' text={user?.displayName}>
+                            <Dropdown.Menu>
+                                <Dropdown.Item as={NavLink} to='/profile' text='My Profile' icon='user' />
+                                <Dropdown.Item onClick={logout} text='Logout' icon='power' />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Menu.Item>
+                </Container>
+            </Menu>
+            <Outlet />
         </>
     )
-}
+})
