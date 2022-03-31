e601aeff7a2d40d08f45ce54f98401e009a1ae42
Stefano Rifici <srifici@technogym.com>
2022-03-31 16:15:52 +0200

    Added Attendees Logic

---
+++ Domain/ActivityAttendee.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain
+{
+    public class ActivityAttendee
+    {
+        public string AppUserId { get; set; }
+
+        public AppUser AppUser { get; set; }
+
+        public Guid ActivityId { get; set; }
+
+        public Activity Activity { get; set; }
+
+        public bool IsHost { get; set; }
+    }
+}
