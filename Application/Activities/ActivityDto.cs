4cfc6dd571f4e9b38b22adc7b8884757002e465e
Stefano Rifici <srifici@technogym.com>
2022-04-14 17:56:42 +0200

    end of section_16

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -15,6 +15,6 @@ namespace Application.Activities
         public string Venue { get; set; }
         public string HostUsername { get; set; }
         public bool IsCanceled { get; set; }
-        public ICollection<Profile> Attendees { get; set; }
+        public ICollection<AttendeeDto> Attendees { get; set; }
     }
 }
--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -14,10 +14,13 @@ namespace Application.Core
                 .ForMember(dest => dest.HostUsername, options => options.MapFrom(src => src.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
                 //.ForMember(dest => dest.Attendees, opt => opt.MapFrom(src => src.Attendees.Select(x => x.AppUser)));
 
-            CreateMap<ActivityAttendee, Profiles.Profile>()
+            CreateMap<ActivityAttendee, AttendeeDto>()
                 .ForMember(dest => dest.DisplayName, options => options.MapFrom(src => src.AppUser.DisplayName))
                 .ForMember(dest => dest.Username, options => options.MapFrom(src => src.AppUser.UserName))
+                .ForMember(dest => dest.Image, options => options.MapFrom(src => src.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(dest => dest.Bio, options => options.MapFrom(src => src.AppUser.Bio));
+
+                CreateMap<AppUser, Profiles.Profile>().ForMember(dest => dest.Image, options => options.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
 
         }
     }
