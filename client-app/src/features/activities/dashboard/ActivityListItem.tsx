71f692cb8a75375cc19eaf8980d7481f1ef53ec0
Stefano Rifici <srifici@technogym.com>
2022-06-15 17:37:57 +0200

    Some Section 17 Stuff

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -26,13 +26,13 @@ export default function ActivityListItem({ activity }: Props) {
                 }
                 <Item.Group>
                     <Item>
-                        <Item.Image style={{marginBottom: 3}} size='tiny' circular src='/assets/user.png' />
+                        <Item.Image style={{marginBottom: 3}} size='tiny' circular src={activity.host?.image || '/assets/user.png'} />
                         <Item.Content>
                             <Item.Header as={Link} to={`/activities/${activity.id}`}>
                                 {activity.title}
                             </Item.Header>
                             <Item.Description>
-                                Hosted by {activity.host?.displayName}
+                                Hosted by <Link to={`/profiles/${activity.hostUsername}`}> {activity.host?.displayName}</Link>
                             </Item.Description>
                             {activity.isHost && (
                                 <Item.Description>
