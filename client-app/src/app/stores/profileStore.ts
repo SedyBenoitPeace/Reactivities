40937809d8d70342264b59966a00651736d5f9b3
Stefano Rifici <srifici@technogym.com>
2022-07-07 23:14:45 +0200

    going on section 17

---
+++ client-app/src/app/stores/profileStore.ts
@@ -0,0 +1,34 @@
+import { makeAutoObservable, runInAction } from "mobx";
+import agent from "../api/agent";
+import { Profile } from "../models/profile";
+import { store } from "./store";
+
+export default class ProfileStore {
+    profile: Profile | null = null;
+    loadingProfile = false;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    get isCurrentUser() {
+        if (store.userStore.user && this.profile) {
+            return store.userStore.user.username === this.profile.username;
+        }
+        return false;
+    }
+    loadProfile = async (username: string) => {
+        this.loadingProfile = true;
+        try {
+            const profile = await agent.Profiles.get(username);
+            runInAction(() => {
+                this.profile = profile;
+                this.loadingProfile = false;
+            }
+            )
+        } catch (error) {
+            console.log(error);
+            runInAction(() => this.loadingProfile = false);
+        }
+    }
+}
