74537ae4d838eec1feb822e08074a448059f14a6
Stefano Rifici <srifici@technogym.com>
2022-07-29 17:20:44 +0200

    Middle of the section

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -10,6 +10,8 @@ namespace Application.Core
     {
         public MappingProfiles()
         {
+            string currentUsername = null;
+
             CreateMap<Activity, Activity>();
             CreateMap<Activity, ActivityDto>()
                 .ForMember(dest => dest.HostUsername, options => options.MapFrom(src => src.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
@@ -19,9 +21,15 @@ namespace Application.Core
                 .ForMember(dest => dest.DisplayName, options => options.MapFrom(src => src.AppUser.DisplayName))
                 .ForMember(dest => dest.Username, options => options.MapFrom(src => src.AppUser.UserName))
                 .ForMember(dest => dest.Image, options => options.MapFrom(src => src.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url))
-                .ForMember(dest => dest.Bio, options => options.MapFrom(src => src.AppUser.Bio));
+                .ForMember(dest => dest.Bio, options => options.MapFrom(src => src.AppUser.Bio))
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.AppUser.Followers.Count))
+                .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.AppUser.Followings.Count))
+                .ForMember(d => d.Following, o => o.MapFrom(s => s.AppUser.Followings.Any(x => x.Observer.UserName == currentUsername)));
 
-            CreateMap<AppUser, Profiles.Profile>().ForMember(dest => dest.Image, options => options.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<AppUser, Profiles.Profile>().ForMember(dest => dest.Image, options => options.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+            .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
+            .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count))
+            .ForMember(d => d.Following, o => o.MapFrom(s => s.Followings.Any(x => x.Observer.UserName == currentUsername)));
 
             CreateMap<Comment, CommentDto>()
             .ForMember(dest => dest.DisplayName, options => options.MapFrom(src => src.Author.DisplayName))
---
+++ Application/Followers/FollowToggle.cs
@@ -0,0 +1,62 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Followers
+{
+    public class FollowToggle
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public string TargetUsername { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly IUserAccessor _userAccessor;
+            private readonly DataContext _context;
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                this._context = context;
+                this._userAccessor = userAccessor;
+
+            }
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var observer = await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                var target = await _context.Users.FirstOrDefaultAsync(x => x.UserName == request.TargetUsername);
+
+                if (target == null) return null;
+
+
+                var following = await _context.UserFollowings.FindAsync(observer.Id, target.Id);
+
+                if (following == null)
+                {
+                    following = new Domain.UserFollowing
+                    {
+                        Observer = observer,
+                        Target = target
+                    };
+
+                    _context.UserFollowings.Add(following);
+                }
+                else
+                {
+                    _context.UserFollowings.Remove(following);
+                }
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Result<Unit>.Success(Unit.Value);
+
+                return Result<Unit>.Failure("Failed on the following");
+            }
+        }
+    }
+}
