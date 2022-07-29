6d1c4c25ba88aa442ba4fc8dd404b21be7ecb015
Stefano Rifici <srifici@technogym.com>
2022-07-29 17:20:44 +0200

    Middle of the section

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,14 +1,12 @@
-using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
-using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -20,8 +18,10 @@ namespace Application.Activities
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
-            public Handler(DataContext context, IMapper mapper)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
+                this._userAccessor = userAccessor;
                 this._mapper = mapper;
                 this._context = context;
             }
@@ -29,7 +29,7 @@ namespace Application.Activities
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, new { currentUsername = _userAccessor.GetUsername()})
                     .ToListAsync();
 
                 return Result<List<ActivityDto>>.Success(activities);
