a5a3e61f8312ad9d60920948f8f6c3e2c69d9a5c
Stefano Rifici <stefano.rifici@gmail.com>
2022-08-14 18:23:45 +0200

    ENd of section 20 before audit fix

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,10 +1,9 @@
-import { formatDistanceToNow, isValid } from 'date-fns';
+import { formatDistanceToNow } from 'date-fns';
 import { Field, FieldProps, Form, Formik } from 'formik';
 import { observer } from 'mobx-react-lite'
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import { Segment, Header, Comment, Button, Loader } from 'semantic-ui-react'
-import MyTextArea from '../../../app/common/form/MyTextInputArea';
+import { Segment, Header, Comment, Loader } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store';
 import * as Yup from 'yup';
 
