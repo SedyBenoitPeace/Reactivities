215ee2f11464d8036a01bae6778e1ca43cf39276
Stefano Rifici <stefano.rifici@gmail.com>
2022-08-20 12:03:16 +0200

    Pre Challenge

---
+++ Application/Activities/ActivityParams.cs
@@ -0,0 +1,14 @@
+using System;
+using Application.Core;
+
+namespace Application.Activities
+{
+    public class ActivityParams : PagingParams
+    {
+
+        public bool IsGoing { get; set; }
+        public bool IsHost { get; set; }
+
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+    }
+}
