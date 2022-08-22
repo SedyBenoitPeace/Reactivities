cce4066aa1d40582388d883f385cc8a8ee9f08e0
Stefano Rifici <srifici@technogym.com>
2022-08-22 12:46:56 +0200

    Pre Token Migration

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -13,5 +13,7 @@ namespace Domain
         public ICollection<UserFollowing> Followings { get; set; }
 
         public ICollection<UserFollowing> Followers { get; set; }
+
+        public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
     }
 }
